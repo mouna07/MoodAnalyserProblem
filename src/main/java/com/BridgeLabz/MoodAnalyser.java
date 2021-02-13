cee68ef1798143695512e1add0baffc00b37a853
mouna07 <mounamc261@gmail.com>
2021-02-13 19:18:01 +0530

    Added: Abilitty to analyse and respond Happy Or Sad

---
+++ src/main/java/com/BridgeLabz/MoodAnalyser.java
@@ -0,0 +1,12 @@
+public class MoodAnalyser {
+
+    public void PrintWelcomeMessage() {
+        System.out.println("Welcome to the Mood Analyser Problem");
+    }
+
+    public String AnalyseMood(String message) {
+        if(message.contains("Sad"))
+            return "SAD";
+        return "HAPPY";
+    }
+
