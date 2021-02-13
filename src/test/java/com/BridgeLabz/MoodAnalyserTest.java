fee4853c9ad67b29a675b4a56cfcd0b9d4ceedbe
mouna07 <mounamc261@gmail.com>
2021-02-13 19:18:01 +0530

    Added: Abilitty to analyse and respond Happy Or Sad

---
+++ src/test/java/com/BridgeLabz/MoodAnalyserTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyserTest
+{
+    @Test
+    public void print_Welcome_Message() {
+        MoodAnalyser mood = new MoodAnalyser();
+        mood.PrintWelcomeMessage();
+    }
+    @Test
+    public void Given_Message_When_Sad_Should_Return_Sad() {
+        MoodAnalyser moodAnalyser= new MoodAnalyser();
+        String mood = moodAnalyser.AnalyseMood("This is a Sad Message");
+        Assert.assertEquals("SAD", mood);
+    }
+
+    @Test
+    public void Given_Message_When_Happy_Should_Return_Happy() {
+        MoodAnalyser moodAnalyser= new MoodAnalyser();
+        String mood = moodAnalyser.AnalyseMood("This is a Happy Message");
+        Assert.assertEquals("HAPPY", mood);
+    }
+}
